140e28db33659f77f738c27e7168a865a0b6e1ad
gokcelb <gokcelbilgin@gmail.com>
2022-02-04 17:29:26 +0300

    Write first three questions' tests and implementations

--- assignment/assignment.go
+++ assignment/assignment.go
@@ -1,15 +1,87 @@
 package assignment
 
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
 func AddUint32(x, y uint32) (uint32, bool) {
-	return 0, false
+	sum := x + y
+	overflow := x > math.MaxUint32-y
+	return sum, overflow
 }
 
-func CeilNumber(f float64) float64 {
-	return 0
+// I wrote two implementations of CeilNumber function to see which was better
+// and the benchmark tests showed me that no string conversion was much more performant
+
+func CeilNumberStringConversion(f float64) float64 {
+	stringifiedFloat := fmt.Sprintf("%v", f)
+	separatedFloat := strings.Split(stringifiedFloat, ".")
+	if len(separatedFloat) == 1 {
+		return f
+	}
+
+	integer, _ := strconv.ParseFloat(separatedFloat[0], 0)
+	decimalPoint, _ := strconv.ParseFloat(separatedFloat[1], 0)
+
+	switch {
+	case decimalPoint > 75:
+		return integer + 1
+	case decimalPoint > 50:
+		return integer + (75 * math.Pow(10, -2))
+	case decimalPoint > 25:
+		return integer + (50 * math.Pow(10, -2))
+	default:
+		return integer + (25 * math.Pow(10, -2))
+	}
+}
+
+func CeilNumberNoStringConversion(f float64) float64 {
+	floored := math.Floor(f)
+	ceiled := math.Ceil(f)
+
+	// return the number as it is if decimal point is equal to
+	// any threshold (i.e. 0.00, 0.25, 0.50, 0.75)
+	if f == floored || f+0.25 == ceiled || f+0.5 == ceiled || f+0.75 == ceiled {
+		return f
+	}
+
+	if f+0.25 > ceiled {
+		return ceiled
+	} else if f+0.5 > ceiled {
+		return floored + 0.75
+	} else if f+0.75 > ceiled {
+		return floored + 0.50
+	} else {
+		return floored + 0.25
+	}
 }
 
 func AlphabetSoup(s string) string {
-	return ""
+	// intiialize zero valued fix-length array based on the constraint
+	// that parameter s will consist of smallcase latin letters
+	var byteList [26]byte
+	// assuming smallcase a will be the smallest
+	smallest := byte(97)
+	// put "a" in the 0th index to form an already sorted array
+	// store how many times the letter shows up as value
+	for i := 0; i < len(s); i++ {
+		byteList[s[i]-smallest] += 1
+	}
+
+	var newWord []byte
+	for i := 0; i < len(byteList); i++ {
+		quantity := int(byteList[i])
+		// append the same letter for as much as its quantity
+		if quantity > 0 {
+			for k := 0; k < quantity; k++ {
+				newWord = append(newWord, byte(i)+smallest)
+			}
+		}
+	}
+	return string(newWord)
 }
 
 func StringMask(s string, n uint) string {
--- assignment/assignment_test.go
+++ assignment/assignment_test.go
@@ -8,45 +8,137 @@ import (
 )
 
 func TestAddUint32(t *testing.T) {
-	/*
-		Sum uint32 numbers, return uint32 sum value and boolean overflow flag
-		cases need to pass:
-			math.MaxUint32, 1 => 0, true
-			1, 1 => 2, false
-			42, 2701 => 2743, false
-			42, math.MaxUint32 => 41, true
-			4294967290, 5 => 4294967295, false
-			4294967290, 6 => 0, true
-			4294967290, 10 => 4, true
-	*/
-	sum, overflow := AddUint32(math.MaxUint32, 1)
+	testCases := []struct {
+		desc             string
+		givenNum1        uint32
+		givenNum2        uint32
+		expectedSum      uint32
+		expectedOverflow bool
+	}{
+		{
+			desc:             "given maxuint32 and 1, expect 0 and overflow true",
+			givenNum1:        math.MaxUint32,
+			givenNum2:        1,
+			expectedSum:      0,
+			expectedOverflow: true,
+		},
+		{
+			desc:             "given 1 and 1, expect 2 and overflow false",
+			givenNum1:        1,
+			givenNum2:        1,
+			expectedSum:      2,
+			expectedOverflow: false,
+		},
+		{
+			desc:             "given 42 and 2701, expect 2743 and overflow false",
+			givenNum1:        42,
+			givenNum2:        2701,
+			expectedSum:      2743,
+			expectedOverflow: false,
+		},
+		{
+			desc:             "given 42 and maxuint32, expect 42 and overflow true",
+			givenNum1:        42,
+			givenNum2:        math.MaxUint32,
+			expectedSum:      41,
+			expectedOverflow: true,
+		},
+		{
+			desc:             "given 4294967290 and 5, expect 4294967295 and overflow false",
+			givenNum1:        4294967290,
+			givenNum2:        5,
+			expectedSum:      4294967295,
+			expectedOverflow: false,
+		},
+		{
+			desc:             "given 4294967290 and 6, expect 0 and overflow true",
+			givenNum1:        4294967290,
+			givenNum2:        6,
+			expectedSum:      0,
+			expectedOverflow: true,
+		},
+		{
+			desc:             "given 4294967290 and 10, expect 4 and overflow true",
+			givenNum1:        4294967290,
+			givenNum2:        10,
+			expectedSum:      4,
+			expectedOverflow: true,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			sum, overflow := AddUint32(tC.givenNum1, tC.givenNum2)
 
-	assert.Equal(t, uint32(0), sum)
-	assert.True(t, overflow)
+			assert.Equal(t, tC.expectedSum, sum)
+			assert.Equal(t, tC.expectedOverflow, overflow)
+		})
+	}
 }
 
-func TestCeilNumber(t *testing.T) {
-	/*
-		Ceil the number within 0.25
-		cases need to pass:
-			42.42 => 42.50
-			42 => 42
-			42.01 => 42.25
-			42.24 => 42.25
-			42.25 => 42.25
-			42.26 => 42.50
-			42.55 => 42.75
-			42.75 => 42.75
-			42.76 => 43
-			42.99 => 43
-			43.13 => 43.25
-	*/
-	point := CeilNumber(42.42)
+func TestCeilNumberStringConversion(t *testing.T) {
+	testCases := formCeilNumTestCases()
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			result1 := CeilNumberStringConversion(tC.givenNum)
 
-	assert.Equal(t, 42.50, point)
+			assert.Equal(t, tC.expectedNum, result1)
+		})
+	}
+}
+
+func TestCeilNumberNoStringConversion(t *testing.T) {
+	testCases := formCeilNumTestCases()
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			result := CeilNumberNoStringConversion(tC.givenNum)
+
+			assert.Equal(t, tC.expectedNum, result)
+		})
+	}
 }
 
 func TestAlphabetSoup(t *testing.T) {
+	testCases := []struct {
+		desc         string
+		givenWord    string
+		expectedWord string
+	}{
+		{
+			desc:         "given hello, expect ehllo",
+			givenWord:    "hello",
+			expectedWord: "ehllo",
+		},
+		{
+			desc:         "given empty string, expect empty string",
+			givenWord:    "",
+			expectedWord: "",
+		},
+		{
+			desc:         "given h, expect h",
+			givenWord:    "h",
+			expectedWord: "h",
+		},
+		{
+			desc:         "given ab, expect ab",
+			givenWord:    "ab",
+			expectedWord: "ab",
+		},
+		{
+			desc:         "given ba, expect ab",
+			givenWord:    "ba",
+			expectedWord: "ab",
+		},
+		{
+			desc:         "given bac, expect abc",
+			givenWord:    "bac",
+			expectedWord: "abc",
+		},
+		{
+			desc:         "given cba, expect abc",
+			givenWord:    "cba",
+			expectedWord: "abc",
+		},
+	}
 	/*
 		String with the letters in alphabetical order.
 		cases need to pass:
@@ -58,9 +150,13 @@ func TestAlphabetSoup(t *testing.T) {
 			"bac" => "abc"
 			"cba" => "abc"
 	*/
-	result := AlphabetSoup("hello")
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			result := AlphabetSoup(tC.givenWord)
 
-	assert.Equal(t, "ehllo", result)
+			assert.Equal(t, tC.expectedWord, result)
+		})
+	}
 }
 
 func TestStringMask(t *testing.T) {
@@ -115,4 +211,70 @@ func TestVariadicSet(t *testing.T) {
 	set := VariadicSet(4, 2, 5, 4, 2, 4)
 
 	assert.Equal(t, []interface{}{4, 2, 5}, set)
+}
+
+type CeilNum struct {
+	desc        string
+	givenNum    float64
+	expectedNum float64
+}
+
+func formCeilNumTestCases() []CeilNum {
+	return []CeilNum{
+		{
+			desc:        "given 42.42, expect 42.50",
+			givenNum:    42.42,
+			expectedNum: 42.50,
+		},
+		{
+			desc:        "given 42, expect 42",
+			givenNum:    42,
+			expectedNum: 42,
+		},
+		{
+			desc:        "given 42.01, expect 42.25",
+			givenNum:    42.01,
+			expectedNum: 42.25,
+		},
+		{
+			desc:        "given 42.24, expect 42.25",
+			givenNum:    42.24,
+			expectedNum: 42.25,
+		},
+		{
+			desc:        "given 42.25, expect 42.25",
+			givenNum:    42.25,
+			expectedNum: 42.25,
+		},
+		{
+			desc:        "given 42.26, expect 42.50",
+			givenNum:    42.26,
+			expectedNum: 42.50,
+		},
+		{
+			desc:        "given 42.55, expect 42.75",
+			givenNum:    42.55,
+			expectedNum: 42.75,
+		},
+		{
+			desc:        "given 42.75, expect 42.75",
+			givenNum:    42.75,
+			expectedNum: 42.75,
+		},
+		{
+			desc:        "given 42.76, expect 43",
+			givenNum:    42.76,
+			expectedNum: 43,
+		},
+		{
+			desc:        "given 42.99, expect 43",
+			givenNum:    42.99,
+			expectedNum: 43,
+		},
+		{
+			desc:        "given 43.13, expect 43.25",
+			givenNum:    43.13,
+			expectedNum: 43.25,
+		},
+	}
 }
